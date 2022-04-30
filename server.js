737e60c2ced6c671cd186c639d579932ce202313
denizmev <dmevlevioglu@gmail.com>
2022-04-30 16:06:03 +0100

    fix temp

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const openai = new OpenAI(process.env.OPENAI_API_KEY);
 const express = require('express')
 const app = express();
 const PORT = process.env.PORT;
-let prompt = "thebot is a no-nonsense, cold, intelligent and pragmatic bot. It will get the job done, but don't expect it to make you laugh. \nhuman: What is the proportion of the Standard Normal Distribution that is below 1.21? \nthebot: The proportion of the Standard Normal Distribution that is below 1.21 is approximately 0.89%. \nhuman: do you enjoy cinema? \nthebot: no. \nhuman: Do a barrel roll. \nthebot: I can't.";
+let prompt = "thebot is a no-nonsense, cold, intelligent and pragmatic bot. It will get the job done, but don't expect it to make you laugh. \nhuman: do you enjoy cinema? \nthebot: no. \nhuman: Do a barrel roll. \nthebot: I can't.";
 
 app.listen(PORT, () => {});
 
@@ -20,11 +20,11 @@ client.on("message", function (message) {
             const gptResponse = await openai.complete({
                 engine: 'text-ada-001',
                 prompt: prompt + `Human: ${message.content}\n` ,
-                temperature: 0.3,
-                max_tokens: 150,
-                top_p: 0.6,
-                frequency_penalty: 0.3,
-                presence_penalty: 0.3,
+                temperature: 0.8,
+                max_tokens: 90,
+                top_p: 0.8,
+                frequency_penalty: 0.6,
+                presence_penalty: 0.6,
                 stop: [" Human:", " AI:"],
             });
             gpt = `${gptResponse.data.choices[0].text}`;
