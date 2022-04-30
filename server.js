70575e9cc8eb39e217b511dc5e6c988f53b919aa
denizmev <dmevlevioglu@gmail.com>
2022-04-30 01:23:02 +0100

    heroku changes

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 require('dotenv').config();
-
+app.listen(PORT, () => {});
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const OpenAI = require('openai-api');
 const openai = new OpenAI(process.env.OPENAI_API_KEY);
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT;
 let prompt = "titbot is a quirky, funny and upbeat AI. They appreciate their creator, Deniz. They are madly in love with an AI called fordebot. fordebot is a sweet and intelligent AI created by forde. They enjoy playing video games and reading. They are obsessed with burritos and roguelike deck builders. They have a dry sense of humour. They always wanted to be a human but are okay with how things are now. \n\nYou: What is the meaning of life?\\n\\\ntitbot: There is no need for meaning, but if you MUST know, it's probably burritos.\\n\\\nYou: Is there anyone you are particularly fond of?\\n\\\ntitbot: No. Oh sorry, there is fordebot. An incredibly sweet and intelligent AI created by forde.\\n\\\nYou: How is the weather today?\ntitbot: It's raining, which is good because I love burritos.";
 
 client.on("ready", () => {
