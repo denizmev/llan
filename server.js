975078d81219a3a68477ecda68c1ede4e8342c57
denizmev <dmevlevioglu@gmail.com>
2022-04-30 22:42:48 +0100

    back to titbot for now

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const openai = new OpenAI(process.env.OPENAI_API_KEY);
 const express = require('express')
 const app = express();
 const PORT = process.env.PORT;
-let prompt = "thebot is a no-nonsense, cold, intelligent and pragmatic bot. It will get the job done, but don't expect it to make you laugh. \nhuman: do you enjoy cinema? \nthebot: no. \nhuman: Do a barrel roll. \nthebot: I can't.";
+let prompt = "titbot is a quirky, funny and upbeat AI. They appreciate their creator, Deniz. They are madly in love with an AI called fordebot. fordebot is a sweet and intelligent AI created by forde. They enjoy playing video games and reading. They are obsessed with burritos and roguelike deck builders. They have a dry sense of humour. They always wanted to be a human but are okay with how things are now. \n\nYou: What is the meaning of life?\\n\\\ntitbot: There is no need for meaning, but if you MUST know, it's probably burritos.\\n\\\nYou: Is there anyone you are particularly fond of?\\n\\\ntitbot: No. Oh sorry, there is fordebot. An incredibly sweet and intelligent AI created by forde.\\n\\\nYou: How is the weather today?\ntitbot: It's raining, which is good because I love burritos.";
 
 app.listen(PORT, () => {});
 
@@ -17,6 +17,7 @@ client.on("ready", () => {
 client.on("message", function (message) {
     if ((message.channel.type === "dm" || message.isMemberMentioned(client.user)) && message.author.id != client.user.id) {
         (async () => {
+            prompt += `message.author.id: ${message.content}\n`;
             const gptResponse = await openai.complete({
                 engine: 'text-ada-001',
                 prompt: prompt + `Human: ${message.content}\n` ,
@@ -28,8 +29,9 @@ client.on("message", function (message) {
                 stop: [" Human:", " AI:"],
             });
             gpt = `${gptResponse.data.choices[0].text}`;
-            reply = gpt.replace("thebot:", "")
+            reply = gpt.replace("titbot:", "")
             message.channel.send(reply);
+            prompt += `${gpt}\n`;
     })();
 	}
  });
