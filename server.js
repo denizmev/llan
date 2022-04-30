0bfe5b914364f303de93f6d22a38fd45ba81f6c7
denizmev <dmevlevioglu@gmail.com>
2022-04-30 15:09:27 +0100

    some changes

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-require('dotenv').config()
+//require('dotenv').config()
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const OpenAI = require('openai-api');
 const openai = new OpenAI(process.env.OPENAI_API_KEY);
-//const express = require('express')
-//const app = express();
-//const PORT = process.env.PORT;
+const express = require('express')
+const app = express();
+const PORT = process.env.PORT;
 let prompt = "thebot is a no-nonsense, cold, intelligent and pragmatic bot. It will get the job done, but don't expect it to make you laugh. \nhuman: What is the proportion of the Standard Normal Distribution that is below 1.21? \nthebot: The proportion of the Standard Normal Distribution that is below 1.21 is approximately 0.89%. \nhuman: do you enjoy cinema? \nthebot: no. \nhuman: Do a barrel roll. \nthebot: I can't.";
 
-//app.listen(PORT, () => {});
+app.listen(PORT, () => {});
 
 client.on("ready", () => {
   client.user.setActivity( "Ask me something!")
